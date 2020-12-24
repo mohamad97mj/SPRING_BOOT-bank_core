1c2051cb6010728b0ba525380ca65bde43897705
mohamad <mohamad97mj@gmail.com>
2020-12-24 17:58:18 +0330

    sd;lfaj;

--- src/main/java/ir/co/pna/exchange/ExchangeApplication.java
+++ src/main/java/ir/co/pna/exchange/ExchangeApplication.java
@@ -24,14 +24,37 @@ public class ExchangeApplication {
     CommandLineRunner lookup(SmsClient quoteClient) {
         return args -> {
             System.out.println("hello there");
-            String mobileNo = "09059242876";
-            String message = "hello";
+            String mobileNo = "09365262198";
+            String message = "زی زی کوچولو، کوچول موچولو";
             SMSGateway gateway = SMSGateway.ADVERTISEMENT;
             String serviceName = "mojahed service";
 
             SendSMSResponse response = quoteClient.sendSms(mobileNo, message, gateway, serviceName);
+
+
+            String mobileNo3 = "09027237097";
+            String message3 = "معاونت محترم طرح و برنامه بانک اقتصاد نوین جناب آقای تولایی با سلام، احتراما به اطلاع می رساند با توجه به استعفای آقای سید محمد حسینی مجاهد از شرکت پرداخت نوین و عدم پایبندی به ددلاین های مشخص شده عدم تمایل شرکت پرداخت نوین به ادامه پروژه سامانه صراقی نوین را اعلام میداریم.";
+            SMSGateway gateway3 = SMSGateway.ADVERTISEMENT;
+            String serviceName3 = "mojahed service";
+
+            SendSMSResponse response3 = quoteClient.sendSms(mobileNo3, message3, gateway3, serviceName3);
+
+
+
+            String mobileNo2 = "09027237097";
+            String message2 = "سلام رییس مجاهد... دیدی بالاخره بعد از هفت خوان رستم این وامونده رو راه انداختم:))";
+            SMSGateway gateway2 = SMSGateway.ADVERTISEMENT;
+            String serviceName2 = "mojahed service";
+
+            SendSMSResponse response2 = quoteClient.sendSms(mobileNo2, message2, gateway2, serviceName2);
+
+
             System.out.println(response.toString());
             System.err.println(response.getSendSMSResult());
+            System.out.println(response2.toString());
+            System.err.println(response2.getSendSMSResult());
+            System.out.println(response3.toString());
+            System.err.println(response3.getSendSMSResult());
         };
     }
 }
