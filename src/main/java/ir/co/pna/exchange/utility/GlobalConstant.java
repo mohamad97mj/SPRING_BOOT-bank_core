07ae66c71009bc4d3d39b2aa75e9dd61f9d564bd
mohamad <mohamad97mj@gmail.com>
2021-01-19 13:48:20 +0330

    num

--- src/main/java/ir/co/pna/exchange/utility/GlobalConstant.java
+++ src/main/java/ir/co/pna/exchange/utility/GlobalConstant.java
@@ -8,7 +8,7 @@ public class GlobalConstant {
     public static final String OPERATIONAL_EXCHANGER_OWNER_BANK_ACCOUNT_ID = "151-701-6113835-1"; // tavalaeeDepositNo
     private static String tavalaeeUsername = "matavallaie";
     private static String tavalaeePassword = "36325045";
-    private static String tavalaeeMobileNumber = "09059242876";
+    private static String tavalaeeMobileNumber = "09027237097";
 
     public static final String OPERATIONAL_EXPORTER_OWNER_BANK_ACCOUNT_ID = "104-701-121924-1"; // yaghliDepositNo
     private static String yaghliUsername = "payamyaghli";
@@ -19,7 +19,7 @@ public class GlobalConstant {
     public static final String OPERATIONAL_CLAIM_OWNER_BANK_ACCOUNT_ID = "147-1-4681241-1"; // shahsavani
     private static String shahsavaniUsername = "da97349734";
     private static String shahsavaniPassword = "85438083";
-    private static String shahsavaniMobileNumber = "09059242876";
+    private static String shahsavaniMobileNumber = "09355772318";
 
     public static final String OPERATIONAL_RETURN_OWNER_BANK_ACCOUNT_ID = "159-701-6471174-1"; // mojahed
     private static String mojahedUsername = "6471174";
